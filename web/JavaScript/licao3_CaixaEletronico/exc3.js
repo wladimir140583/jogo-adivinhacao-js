64badfaa56a026b219bae7e509d268964cac045e
engTx <engenharia.transmissaosp@gmail.com>
2026-02-05 21:31:12 -0300

    Adiciona atividade: caixa eletrônico

---
+++ web/JavaScript/licao3_CaixaEletronico/exc3.js
@@ -0,0 +1,14 @@
+function calcularNotas () {
+
+    // Passo 1 criar referencia aos Elementos da página
+    let inSaque = document.getElementById("inSaque");
+    let outNotasCem = document.getElementById("outNotasCem");
+    let outNotasCinquena = document.getElementById("outNotasCinquena");
+    let outNotasDez = document.getElementById("outNotasDez");
+
+    // Limpa mensagem (caso, segunda execução)
+    outNotasCem = "";
+    outNotasCinquena = "";
+    outNotasDez = "";
+
+}
